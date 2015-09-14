f1a84087b592e3cba681c103e8a22aef250b8138
liuyong <bjliuyong@jd.com>
2015-09-14 18:29:57 +0800

    add plugin

--- easy4j-framework-web/src/main/java/org/easy4j/framework/web/startup/config/WebMvcConfig.java
+++ easy4j-framework-web/src/main/java/org/easy4j/framework/web/startup/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package org.easy4j.framework.web.startup.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -14,5 +15,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc          //<mvc:annotation-driven />
 @ImportResource({"classpath:spring-mvc.xml"})
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        System.out.println("=====addInterceptors====");
+
+    }
 
 }
