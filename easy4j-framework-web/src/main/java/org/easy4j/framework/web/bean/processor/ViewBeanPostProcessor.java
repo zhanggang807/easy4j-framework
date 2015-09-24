ef0963d9119660a7f7038a10583c01172d1083e2
liuyong <bjliuyong@jd.com>
2015-09-24 17:48:15 +0800

    add plugin

--- easy4j-framework-web/src/main/java/org/easy4j/framework/web/bean/processor/ViewBeanPostProcessor.java
+++ easy4j-framework-web/src/main/java/org/easy4j/framework/web/bean/processor/ViewBeanPostProcessor.java
@@ -4,7 +4,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -65,13 +64,51 @@ public class ViewBeanPostProcessor implements BeanPostProcessor,ApplicationConte
         BeanDefinitionBuilder viewResolverBuilder = BeanDefinitionBuilder.rootBeanDefinition(VelocityLayoutViewResolver.class);
         BeanDefinitionBuilder velocityConfigurerBuilder = BeanDefinitionBuilder.rootBeanDefinition(VelocityConfigurer.class);
 
-        Properties velocityProperties = new Properties();
+        Properties velocityProperties = loadDefaultVelocityConfigFile();
 
-        String outputEncoding = "utf-8";
+        velocityProperties.setProperty(SpringResourceLoader.SPRING_RESOURCE_LOADER_CACHE, "true");
 
-        velocityProperties.setProperty("input.encoding","utf-8");
-        velocityProperties.setProperty("output.encoding",outputEncoding);
-        velocityProperties.setProperty(SpringResourceLoader.SPRING_RESOURCE_LOADER_CACHE, "false");
+        mergeVelocityConfigFile(velocityProperties);
+
+        velocityConfigurerBuilder.addPropertyValue("velocityProperties",velocityProperties) ;
+
+        String resourceLoaderPath = velocityProperties.getProperty("resourceLoaderPath", "classpath:tpl/");
+        velocityConfigurerBuilder.addPropertyValue("resourceLoaderPath",resourceLoaderPath) ; //WEB-INF/vm
+        velocityConfigurerBuilder.addPropertyValue("preferFileSystemAccess",false) ; //WEB-INF/vm
+
+        ClassPathResource macroResource = new ClassPathResource("tpl/macro.vm");
+        if(macroResource.exists()){
+            velocityProperties.setProperty("velocimacro.library","macro.vm");
+        }
+
+        /* defualt<property name="exposeSpringMacroHelpers" value="false"/>*/
+        /* defualt <property name="layoutKey" value="layout"/>*/
+        /* defualt <property name="screenContentKey" value="screen_content"/>*/
+        viewResolverBuilder.addPropertyValue("contentType","text/html;charset=" + velocityProperties.getProperty("output.encoding"));
+        viewResolverBuilder.addPropertyValue("suffix",velocityProperties.getProperty("suffix",".vm")); //layoutViewResolver.setSuffix(".vm");
+        viewResolverBuilder.addPropertyValue("layoutKey",velocityProperties.getProperty("suffix","layout"));
+        viewResolverBuilder.addPropertyValue("exposePathVariables",velocityProperties.getProperty("exposePathVariables", "true")); //layoutViewResolver.setExposePathVariables(true);
+
+        listableBeanFactory.registerBeanDefinition("viewResolver",viewResolverBuilder.getBeanDefinition());
+
+        listableBeanFactory.registerBeanDefinition("velocityConfigurer",velocityConfigurerBuilder.getBeanDefinition());
+    }
+
+    private Properties loadDefaultVelocityConfigFile(){
+
+        ClassPathResource resource = new ClassPathResource("velocity.properties", ViewBeanPostProcessor.class);
+        try {
+            return PropertiesLoaderUtils.loadProperties(resource);
+        } catch (IOException e) {
+            throw  new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 如果classpath 下面有velocity.properties 文件 将其进行合并， 并且覆盖默认值
+     * @param velocityProperties
+     */
+    private void mergeVelocityConfigFile(Properties velocityProperties){
 
         ClassPathResource classPathResource = new ClassPathResource("velocity.properties");
         if(classPathResource.exists()){
@@ -82,22 +119,6 @@ public class ViewBeanPostProcessor implements BeanPostProcessor,ApplicationConte
                 new RuntimeException(e);
             }
         }
-        //velocityProperties.setProperty("velocimacro.library","macro.vm");
-        velocityConfigurerBuilder.addPropertyValue("velocityProperties",velocityProperties) ;
-        velocityConfigurerBuilder.addPropertyValue("resourceLoaderPath",velocityProperties.getProperty("resourceLoaderPath","classpath:tpl/")) ; //WEB-INF/vm
-        velocityConfigurerBuilder.addPropertyValue("preferFileSystemAccess",false) ; //WEB-INF/vm
-
-        /* defualt<property name="exposeSpringMacroHelpers" value="false"/>*/
-        /* defualt <property name="layoutKey" value="layout"/>*/
-        /* defualt <property name="screenContentKey" value="screen_content"/>*/
-        viewResolverBuilder.addPropertyValue("contentType","text/html;charset=" + velocityProperties.getProperty("output.encoding",outputEncoding));
-        viewResolverBuilder.addPropertyValue("suffix",velocityProperties.getProperty("suffix",".vm")); //layoutViewResolver.setSuffix(".vm");
-        viewResolverBuilder.addPropertyValue("layoutKey",velocityProperties.getProperty("suffix","layout"));
-        viewResolverBuilder.addPropertyValue("exposePathVariables",velocityProperties.getProperty("exposePathVariables","true")); //layoutViewResolver.setExposePathVariables(true);
-
-        listableBeanFactory.registerBeanDefinition("viewResolver",viewResolverBuilder.getBeanDefinition());
-
-        listableBeanFactory.registerBeanDefinition("velocityConfigurer",velocityConfigurerBuilder.getBeanDefinition());
     }
 
     @Override
