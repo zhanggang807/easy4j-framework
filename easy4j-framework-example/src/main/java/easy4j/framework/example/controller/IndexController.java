2eb93950005a0879fd50ec32206d280e85918f5c
liuyong <bjliuyong@jd.com>
2015-10-16 10:53:53 +0800

    add plugin

--- easy4j-framework-example/src/main/java/easy4j/framework/example/controller/IndexController.java
+++ easy4j-framework-example/src/main/java/easy4j/framework/example/controller/IndexController.java
@@ -1,11 +1,14 @@
 package easy4j.framework.example.controller;
 
+import com.google.common.collect.Maps;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * @author bjliuyong
@@ -32,6 +35,22 @@ public class IndexController {
     @RequestMapping(value = "/vm")
     public String webInf(HttpServletRequest request ,HttpServletResponse response) throws Exception{
         return request.getParameter("vm");
+    }
+
+
+    @RequestMapping(value = "/json")
+    public @ResponseBody Map json(HttpServletRequest request ,HttpServletResponse response) throws Exception{
+        Map map = Maps.newHashMap();
+        map.put("code",1);
+        return map;
+
+    }
+
+    @RequestMapping(value = "/str")
+    public @ResponseBody String  str(HttpServletRequest request ,HttpServletResponse response) throws Exception{
+
+        return "this is string 你好";
+
     }
 
 
--- easy4j-framework-web/src/main/java/org/easy4j/framework/web/startup/config/WebMvcConfig.java
+++ easy4j-framework-web/src/main/java/org/easy4j/framework/web/startup/config/WebMvcConfig.java
@@ -2,9 +2,13 @@ package org.easy4j.framework.web.startup.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,8 +31,40 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 
-        System.out.println("=====configureMessageConverters====");
+        /*StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(Charset.forName("utf-8"));
+        converters.add(stringHttpMessageConverter);*/
+    }
 
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        int len = converters.size();
+
+        for(int index = 0 ; index < len ; index++ ){
+            if(converters.get(index) instanceof StringHttpMessageConverter ) {
+                extendStringHttpMessageConverter(converters ,index);
+            }
+        }
+
+
+    }
+
+
+    //设置StringHttpMessageConverter编码，防止乱码
+    private void extendStringHttpMessageConverter(List<HttpMessageConverter<?>> converters , int position ){
+        Charset defaultCharset = Charset.forName("utf-8");
+        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
+
+        List<MediaType> mediaTypeList = new ArrayList<MediaType>(3);
+
+        mediaTypeList.add(new MediaType("text", "html", defaultCharset));
+        mediaTypeList.add(new MediaType("text", "plain", defaultCharset));
+        mediaTypeList.add(new MediaType("*", "*", defaultCharset));
+
+        stringHttpMessageConverter.setSupportedMediaTypes(mediaTypeList);
+        stringHttpMessageConverter.setWriteAcceptCharset(false);
+        converters.remove(position);
+        converters.add(position,stringHttpMessageConverter);
     }
 
 
