92b87ed2d94a4c3aacf1559bc1fbff7a1941046e
liuyong <bjliuyong@jd.com>
2015-10-16 15:29:40 +0800

    add plugin

---
+++ easy4j-framework-core/src/main/java/org/easy4j/framework/core/config/GlobalConstants.java
@@ -0,0 +1,38 @@
+package org.easy4j.framework.core.config;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Properties;
+
+/**
+ * @author bjliuyong
+ * @version 1.0
+ * @created date 15-10-16
+ */
+public class GlobalConstants {
+
+    public static Properties easy4jProperties = new Properties();
+
+    private static final String DEFAULT_CHARSET_NAME = "UTF-8";
+
+    public static final String  CHARSET_NAME  = getString("project.charset.name",DEFAULT_CHARSET_NAME) ;
+    public static final Charset CHARSET       =  Charset.forName(CHARSET_NAME);
+
+    static {
+
+        try {
+            easy4jProperties.load(GlobalConstants.class.getResourceAsStream("easy4j.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException("can not find easy4j.properties from classpath");
+        }
+
+    }
+
+    public static String getString(String key,String defaultVal){
+        if(easy4jProperties == null)
+            return defaultVal ;
+
+        String val = easy4jProperties.getProperty(key);
+        return val == null ? defaultVal : val ;
+    }
+}
