0c266743c5fb50bf06024692f3c33c5901a32681
liuyong <bjliuyong@jd.com>
2015-11-03 13:03:26 +0800

    add sql builder

--- easy4j-framework-core/src/main/java/org/easy4j/framework/core/orm/SQLBuilder.java
+++ easy4j-framework-core/src/main/java/org/easy4j/framework/core/orm/SQLBuilder.java
@@ -16,34 +16,37 @@ public class SQLBuilder {
     public static final String ALL_COLUMNS = null ;
 
     public static String generateSelectSQL(String columns ,String tableName ,String conditions){
-        AbstractSQL sql = new SelectSQL(tableName);
-        sql.appendColumns(columns);
-        sql.appendConditions(conditions);
-        return sql.build();
+        if(columns == null || columns.length() == 0) {
+            return join("select * from",tableName ,conditions);
+        }
+
+        return join("select",columns ,"from" ,tableName ,conditions);
     }
 
+
     public static String generateSelectCountSQL(String tableName ,String conditions){
-        StringBuilder sql = new StringBuilder("SELECT COUNT(1) FROM ");
-        sql.append(tableName);
-        appendConditions(sql,conditions);
-        return sql.toString();
+        return join("select count(1) from" ,tableName ,conditions);
     }
 
     public static String generateSelectSqlForPager(String columns ,String tableName ,String conditions ,int pageNumber,int pageSize ){
-        SelectSQL sql = new SelectSQL(tableName);
-        sql.appendColumns(columns);
-        sql.appendConditions(conditions);
-        sql.appendSqlForPager(pageNumber,pageSize);
-        return sql.build();
+
+        int start = 0 ;
+
+        if(pageNumber > 1)
+            start = (pageNumber - 1) * pageSize ;
+
+        StringBuilder limitBuilder = new StringBuilder(5);
+        limitBuilder.append("limit ").append(start).append(',').append(pageNumber);
+
+        if(columns == null || columns.length() == 0) {
+            return join("select * from",tableName,conditions,limitBuilder.toString());
+        }
+        return join("select",columns,"from",tableName,conditions,limitBuilder.toString());
     }
 
+
     public static String generateUpdateSQL(String tableName,String sets ,String conditions){
-
-        if(conditions == null || conditions.length() == 0)
-            return  Strings.join("UPDATE ",tableName," SET ",sets ) ;
-        else
-            return  Strings.join("UPDATE ",tableName," SET ",sets ," WHERE ",conditions) ;
-
+        return  join("update",tableName,"set",sets ,conditions) ;
     }
 
     public static String generateInsertSQL(String tableName,Object ... columns){
@@ -67,36 +70,54 @@ public class SQLBuilder {
     }
 
     public static String generateDeleteSQL(String tableName,String conditions){
-
-        if(conditions == null || conditions.length() == 0)
-            return Strings.join("delete from ",tableName );
-        else
-            return Strings.join("delete from ",tableName ," where ",conditions);
+        return join("delete from",tableName ,conditions);
     }
 
+    public static String join(String ... params){
+        int len =  0 ;
 
+        for (String str : params){
 
-    public static void appendConditions(StringBuilder sqlBuilder,String conditions){
-        if(conditions != null && !conditions.isEmpty()){
-            sqlBuilder.append(" WHERE ").append(conditions);
+            if(str == null || str.length() == 0)
+                continue;
+
+            len = len + 1 + str.length() ; //1 for ' '
         }
-    }
+        char[] chars = new char[len];
 
+        int pos = 0 ;
+        for(String str : params){
+
+            if(str == null || str.length() == 0)
+                continue;
+            int l = str.length();
+            str.getChars(0, l, chars, pos);
+            pos += l ;
+            chars[pos++] = ' ';
+        }
+
+        return new String(chars);
+    }
 
     public static void main(String args[]){
-        System.out.println(generateSelectSQL("","user","id=?"));
-        System.out.println(generateSelectSQL(null,"user","id=?"));
+
+        System.out.println(join("select","age,sex","from","t1"));
+
+        System.out.println(generateSelectSQL("","user","where id=?"));
+        System.out.println(generateSelectSQL(null,"user","where id=?"));
         System.out.println(generateSelectSQL("id,name,age","user",""));
         System.out.println(generateSelectSQL("id,name,age", "user", null));
         System.out.println(generateDeleteSQL("user", null));
-        System.out.println(generateDeleteSQL("user", "id=?"));
+        System.out.println(generateDeleteSQL("user", "where id=?"));
         System.out.println(generateInsertSQL("user", "id", "name", "age"));
         Map<String,String> fieldMapping = new LinkedHashMap<String, String>(10);
         fieldMapping.put("id","id");
         fieldMapping.put("name","name");
         fieldMapping.put("age","age");
 
-        System.out.println(generateInsertSQL("user", (String[])fieldMapping.values().toArray()));
+        System.out.println(generateInsertSQL("user", fieldMapping.values().toArray()));
     }
+
+
 
 }
