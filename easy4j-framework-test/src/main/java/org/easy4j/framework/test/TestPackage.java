ba7d94a4953d346a085958faa19709aa3bf346bf
liuyong <bjliuyong@jd.com>
2015-11-10 13:06:55 +0800

    add sql builder

--- easy4j-framework-test/src/main/java/org/easy4j/framework/test/TestPackage.java
+++ easy4j-framework-test/src/main/java/org/easy4j/framework/test/TestPackage.java
@@ -1,14 +1,14 @@
 package org.easy4j.framework.test;
 
 import org.easy4j.framework.core.util.ArrayUtils;
-import org.easy4j.framework.web.startup.config.DefualtScanPackage;
+import org.easy4j.framework.web.startup.config.DefaultScanPackage;
 
 /**
  * @author bjliuyong
  * @version 1.0
  * @created date 15-11-9
  */
-public class TestPackage  extends DefualtScanPackage  {
+public class TestPackage  extends DefaultScanPackage {
 
     @Override
     public String[] getBasePackages() {
