6815ebe88384ebe9f7403265bb51e5d8e8457b87
bjliuyong <liuyong1352@126.com>
2015-11-22 22:08:10 +0800

    orm annotation

---
+++ easy4j-framework-core/src/main/java/org/easy4j/framework/core/orm/page/Pager.java
@@ -0,0 +1,58 @@
+package org.easy4j.framework.core.orm.page;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author: liuyong
+ * @since 1.0
+ */
+public class Pager<E> extends ArrayList<E> implements List<E> {
+
+    private int pageNumber ;
+
+    private int total ;
+
+    private int pageSize ;
+
+    public Pager(Collection<? extends E> c,int pageNumber , int pageSize ,int total ){
+        super(c);
+        this.pageNumber = pageNumber ;
+        this.pageSize = pageSize ;
+        this.total = total ;
+    }
+
+    public boolean isFirst(){
+        return pageNumber == 0 ;
+    }
+
+    public boolean isLast(){
+        return pageNumber == total / pageSize ;
+    }
+
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+}
