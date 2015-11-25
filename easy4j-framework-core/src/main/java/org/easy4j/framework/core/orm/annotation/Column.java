b5de6683a370b8ca6b9f3939baa0c5f0fd49999f
bjliuyong <liuyong1352@126.com>
2015-11-25 21:24:08 +0800

    add some column annotation feild

--- easy4j-framework-core/src/main/java/org/easy4j/framework/core/orm/annotation/Column.java
+++ easy4j-framework-core/src/main/java/org/easy4j/framework/core/orm/annotation/Column.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p>定义列名</p>
+ * <p>custom field mapping  the column name  in database </p>
  *
  * @author: liuyong
  * @since 1.0
@@ -15,7 +15,19 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Column {
 
+    /**
+     *
+     * this field present the column name in database
+     *
+     * @return
+     */
     String value() default "";
 
+    /**
+     *
+     * if ignore is true , this field will ignore , database operation will ignore it
+     *
+     * @return
+     */
     boolean ignore() default false ;
 }
